96f0e68b8f4321dd2959d1f1fb1aea78cbfe23c3
chloegem <chloegemayel@Chloes-MacBook-Pro.local>
2022-12-15 22:10:30 +0200

    Home page UI/UX complete

--- Frontend/app/src/main/java/com/example/glow/HomeActivity.java
+++ Frontend/app/src/main/java/com/example/glow/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.glow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+    
+    public void goWater(View view){
+    }
+    public void goFood(View view){
+    }
+    public void goSport(View view){
+    }
+    public void goUser(View view){
     }
 }
