435f5a24407b61a63613a0ed234652f8c8d4e07c
chloegem <chloegemayel@Chloes-MacBook-Pro.local>
2022-12-15 03:37:17 +0200

    Login API

---
+++ Backend/login.php
@@ -0,0 +1,50 @@
+<?php
+ 
+ include "connection.php";
+ 
+ function validate($data){
+     $data = trim($data);
+     $data = stripslashes($data);
+     return $data;
+ }
+ if (isset($_POST['email']) && isset($_POST['password'])) {
+ 
+ $email = validate($_POST['email']);
+ $password = mysqli_real_escape_string($mysqli, stripslashes(htmlspecialchars($_POST['password'])));
+ 
+ 
+     if (empty($email)) {
+     echo("User Name is required");
+     exit();
+     }
+     else if(empty($password)){
+         echo("Password is required");
+         exit();
+     }
+     else{
+         $query = $mysqli->prepare("SELECT * FROM users WHERE email = ?;");
+         $query->bind_param("s",$email);
+         $query->execute();
+         if($query->num_rows == 0){
+             $array = $query->get_result();
+             $row = $array->fetch_assoc();
+             $dbemail = $row['email'];
+             $dbpassword = $row['password'];
+             $response = [];
+             if ($dbemail == $email && password_verify($password, $dbpassword)) {
+                 $response[] = $row;
+                 $json_respnse = json_encode($response);
+                 echo $json_respnse;
+                 exit();
+             }   
+             else{
+                 echo("Incorrect Username or password");
+                 exit();
+             }
+         }
+         else{
+             echo("Incorrect Username or password");
+             exit();
+         }
+     }
+ }
