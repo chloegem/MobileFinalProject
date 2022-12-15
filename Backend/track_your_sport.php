14383626f307c3a56b8664094a9b583a2c5a1292
chloegem <chloegemayel@Chloes-MacBook-Pro.local>
2022-12-15 04:02:36 +0200

    Tracking Sport API

---
+++ Backend/track_your_sport.php
@@ -0,0 +1,27 @@
+<?php
+ 
+ include "connection.php";
+ 
+ if (isset($_POST['user_id']) && isset($_POST['date'])) {
+     $user = $_POST['user_id'];
+     $date = $_POST['date'];
+ 
+     if (empty($user)) {
+         exit();
+     }else if(empty($date)){
+         exit();
+     }else{
+         $sql = "SELECT * FROM exercises WHERE user_id = '" . $user . "' AND date = " . $date . "";
+         $result = mysqli_query($mysqli, $sql);
+ 
+         if (mysqli_num_rows($result) != 0) {
+             $raw = mysqli_fetch_assoc($result);
+             $response = [];
+             $response[] = $raw;
+             $json_respnse = json_encode($response);
+             echo $json_respnse;
+         }else{
+             echo "No data";
+         }
+     }
+ }  
