7c0503e0c5ef75a597a1f8b9f9a28c4dff109c7c
chloegem <chloegemayel@Chloes-MacBook-Pro.local>
2022-12-15 13:46:01 +0200

    Splash Screen Activity complete

--- Frontend/app/src/main/java/com/example/glow/MainActivity.java
+++ Frontend/app/src/main/java/com/example/glow/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.glow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread thread = new Thread() {
+
+            public void run(){
+                try{
+                    sleep(2000);
+                }catch(Exception e){
+                    e.printStackTrace();
+                }finally{
+                    startActivity(new Intent(MainActivity.this,Login.class));
+                }
+            }
+        };
+        thread.start();
     }
 }
