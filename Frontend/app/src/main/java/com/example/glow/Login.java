ada3056599064ca2d48aca552c3f5808ed0531d6
chloegem <chloegemayel@Chloes-MacBook-Pro.local>
2022-12-15 18:30:26 +0200

    Completed Register UI/UX

--- Frontend/app/src/main/java/com/example/glow/Login.java
+++ Frontend/app/src/main/java/com/example/glow/Login.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -115,5 +116,22 @@ public class Login extends AppCompatActivity {
         password = (EditText) findViewById(R.id.password);
         btn = (Button) findViewById(R.id.loginBtn);
         text = (TextView) findViewById(R.id.textView);
+    }
+
+    public void register_onclick(View view){
+        startActivity(new Intent(Login.this, Register.class));
+    }
+
+    public void login_onclick(View view) {
+        String input_email = email.getText().toString();
+        String input_password = password.getText().toString();
+
+        if (input_email.equals("") || input_password.equals("")) {
+            Toast.makeText(getApplicationContext(), "Missing Info", Toast.LENGTH_LONG).show();
+        } else {
+            String url = "http://192.168.106.1/MobileFinal/Backend/login.php";
+            DownloadTask task = new DownloadTask();
+            task.execute(input_email, input_password, url);
+        }
     }
 }
---
+++ Frontend/app/src/main/java/com/example/glow/Register.java
@@ -0,0 +1,17 @@
+package com.example.glow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class Register extends AppCompatActivity {
+
+    EditText fname_register, lname_register, 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
